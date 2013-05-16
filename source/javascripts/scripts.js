8bd0650833c4b7a2b7735f2d8e8dde2cbb461994
alauderdale <alauderdale@mac.com>
2013-05-15 23:38:02 -0600

    slider

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  console.log('hello');
+  $('#full-width-slider').royalSlider({
+    loop: true,
+    keyboardNavEnabled: true,
+    imageScaleMode: 'none',
+    autoScaleSlider: false, 
+    controlNavigation: 'bullets',
+    thumbsFitInViewport: false,
+    navigateByClick: true,
+    startSlideId: 0,
+    autoPlay: false,
+    transitionType:'move',
+    globalCaption: true
+  });
 });
